2e5ce456be9b7f5b8f3c52c602982e7ae8c79cf5
Brandon Bode <brandonbode25@gmail.com>
2024-08-26 21:57:03 -0400

    Updated App.js to include new Pokedex component. Added styles to App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import Pokecard from './Pokecard';
+import React, { Component } from 'react'
+import Pokedex from './Pokedex';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <Pokecard
-                id={4}
-                name="Charmander"
-                type="fire"
-                exp={62}
-            />
+            <Pokedex />
         </div>
     );
 }
